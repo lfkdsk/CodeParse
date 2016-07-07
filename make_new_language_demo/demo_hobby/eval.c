b2b1fed8e5613b1a6c4a947e32a2179e44f42c85
lfkdsk <lfk_dsk@hotmail.com>
2016-07-07 08:35:53 +0800

    update

--- make_new_language_demo/demo_hobby/eval.c
+++ make_new_language_demo/demo_hobby/eval.c
@@ -115,3 +115,61 @@ static HBB_Value eval_identifier_expression(HBB_Interpreter *inter, LocalEnviron
 
         return v;
 }
+
+static HBB_Value eval_expression(HBB_Interpreter *inter, LocalEnvironment *env,
+                                  char *identifier, Expression *expression);
+
+static HBB_Value eval_assign_expression(HBB_Interpreter *inter, LocalEnvironment *env,
+                                        char *identifier, Expression *expression){
+        HBB_Value v;
+        Variable *left;
+
+        v = eval_expression(inter, env, expression);
+
+        left = hbb_search_local_variable(env, identifier);
+
+        if (left == NULL) {
+                left = search_global_variable_from_env(inter, env, identifier);
+        }
+
+        if (left != NULL) {
+                release_if_string(&left->value);
+                left->value = v;
+                refer_if_string(&v);
+        } else {
+                if (env != NULL) {
+                        hbb_and_local_variable(env, identifier, &v);
+                } else {
+                        HBB_and_global_variable(inter, identifier, &v);
+                }
+
+                refer_if_string(&v);
+        }
+        return v;
+}
+
+/**
+ * 运行布尔表达式
+ * @param  inter       [description]
+ * @param  operator    [description]
+ * @param  left        [description]
+ * @param  right       [description]
+ * @param  line_number [description]
+ * @return             [description]
+ */
+static HBB_Boolean eval_binary_boolean(HBB_Interpreter *inter, ExpressionType operator,
+                                       HBB_Boolean left, HBB_Boolean right, int line_number){
+        HBB_Boolean result;
+
+        if (operator == EQ_EXPRESSION) {
+                result = left == right;
+        } else if (operator == NE_EXPRESSION) {
+                result = left != right;
+        } else {
+                char *op_str = hbb_get_operator_string(operator);
+                hbb_runtime_error(line_number, NOT_BOOLEAN_OPERATOR_ERROR,
+                                  STRING_MESSAGE_ARGUMENT, "operator", op_str, MESSAGE_ARGUMENT_END);
+        }
+
+        return result;
+}
