673a0ef858e9408dbf17994b85a750a40a6fdf11
lfkdsk <lfk_dsk@hotmail.com>
2016-07-16 11:11:49 +0800

    add function

--- hobby_script/src/HobbyScript/Ast/FuncStmt.java
+++ hobby_script/src/HobbyScript/Ast/FuncStmt.java
@@ -5,10 +5,30 @@ import HobbyScript.Token.HobbyToken;
 import java.util.List;
 
 /**
- * Created by liufengkai on 16/7/15.
+ * 函数
+ *
+ * @author liufengkai
+ *         Created by liufengkai on 16/7/15.
  */
 public class FuncStmt extends AstList {
     public FuncStmt(List<AstNode> children) {
         super(children, HobbyToken.FUNCTION);
+    }
+
+    public String name() {
+        return ((AstLeaf) child(0)).token().getText();
+    }
+
+    public ParameterList parameters() {
+        return (ParameterList) child(1);
+    }
+
+    public BlockStmnt body() {
+        return (BlockStmnt) child(2);
+    }
+
+    public String toString() {
+        return "(func " + name() + " " + parameters() + " "
+                + body() + " )";
     }
 }
